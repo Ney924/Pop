e1be5e992575a81d86048b39b14f8e163e0f7557
Aksenov <ais924@yandex.ru>
2021-04-01 15:36:04 +0300

    game 1.3 лопанье шариков (рабочее)

--- game.js
+++ game.js
@@ -1,115 +1,121 @@
 const params = {
-	width: 800,
-	height: 600,
-	widthBall: 30,
+   width: 800,
+   height: 600,
+   widthBall: 30,
+   widthNeedle: 80,
 };
 
-const game = {          																		// Глобальная переменная
-	ctx: undefined,																				// Контекст
-	sprites: {
-		background: undefined,
-		ball: undefined,
-		needle: undefined,
-	},
-	state: {																					// Физика
-		balls: [],
-		needle: {
-			x: 360,
-			y: 0,
-			velocity: 6,
-			dx: 0,
-			move: function () {
-				this.x += this.dx;
-			},
-			stop: function () {
-				this.dx = 0;
-			}
-		}
-	},
+const sprites = {
+   background: undefined,
+   ball: undefined,
+   needle: undefined,
+};
 
-	init: function () {
-		const canvas = document.getElementById('canvas');
-		this.ctx = canvas.getContext("2d");
-		window.addEventListener("keydown", function (e) {
-			if (e.keyCode == 37) {
-				game.state.needle.dx = -game.state.needle.velocity;
-			} else if (e.keyCode == 39) {
-				game.state.needle.dx = game.state.needle.velocity;
-			}
-		});
-		window.addEventListener("keyup", function (e) {
-			game.state.needle.stop();
-		});
-	},
+const state = {
+   balls: [],
+   needle: {
+      x: 360,
+      y: 0,
+      velocity: 6,
+      dx: 0,
+      move: function () {
+         this.x += this.dx;
+      },
+      stop: function () {
+         this.dx = 0;
+      }
+   }
+};
 
-	load: function () {
+const init = function () {
+   const canvas = document.getElementById('canvas');
+   ctx = canvas.getContext("2d");
+   window.addEventListener("keydown", function (e) {
+      if (e.keyCode == 37) {
+         state.needle.dx = -state.needle.velocity;
+      } else if (e.keyCode == 39) {
+         state.needle.dx = state.needle.velocity;
+      }
+   });
+   window.addEventListener("keyup", function (e) {
+      state.needle.stop();
+   });
+};
 
-		this.sprites.background = new Image();
-		this.sprites.background.src = 'images/background.png';									// Загружаем из объекта sprites картинки для отрисовки
+const load = function () {
+   sprites.background = new Image();
+   sprites.background.src = 'images/background.png';								// Загружаем из объекта sprites картинки для отрисовки
 
-		this.sprites.ball = new Image();
-		this.sprites.ball.src = 'images/ball.png';												// Загружаем из объекта sprites картинки для отрисовки
+   sprites.ball = new Image();
+   sprites.ball.src = 'images/ball.png';												// Загружаем из объекта sprites картинки для отрисовки
 
-		this.sprites.needle = new Image();
-		this.sprites.needle.src = 'images/needle.png';											// Загружаем из объекта sprites картинки для отрисовки
+   sprites.needle = new Image();
+   sprites.needle.src = 'images/needle.png';											// Загружаем из объекта sprites картинки для отрисовки
+};
 
-	},
+const createBall = function () {                                           //Создаём шарики в массиве шары в state 
+   state.balls.push({
+      x: Math.random() * (params.width - params.widthBall - 0),
+      y: params.height,
+      dy: -2,
+      dell: false,
+      flight: function () {
+         this.y += this.dy;
+      }
+   })
+};
 
-	createBall: function () {
-		for (let i = 0; i <= 2; i++) {
-			this.state.balls.push({
-				x: Math.random() * (params.width - params.widthBall - 0),
-				y: 300,
-				dy: -2,
-				flight: function () {
-					this.y += this.dy;
-				}
-			})
+const dellBall = function () {                                              // Удаление шариков
+   for (let j = 0; j < state.balls.length; j++) {
+      if (Math.abs((state.needle.x) - state.balls[j].x) < 55 && state.balls[j].y < 100) {
+         state.balls.splice(j, 1)
+      }
+   }
+};
 
-		}
-	},
+console.log(state.needle.x - params.widthNeedle / 2);
 
-	start: function () {
-		this.init();
-		this.load();
-		this.createBall();
-		this.spawnInteral();
-		this.run();
-	},
+const start = function () {
+   init();
+   load();
+   createBall();
+   dellBall();
+   run();
+};
 
-	render: function () { 																					// Рендерим наши объекты
-		this.ctx.clearRect(0, 0, this.width, this.height);
-		this.ctx.drawImage(this.sprites.background, 0, 0);
-		this.ctx.drawImage(this.sprites.needle, this.state.needle.x, this.state.needle.y, 80, 40);
-		for (let i = 0; i < this.state.balls.length; i++) {
-			this.ctx.drawImage(this.sprites.ball, this.state.balls[i].x, this.state.balls[i].y, 30, 40);
-		}
-	},
+const render = function () { 																					// Рендерим все наши объекты
+   ctx.clearRect(0, 0, params.width, params.height);
+   ctx.drawImage(sprites.background, 0, 0);
+   ctx.drawImage(sprites.needle, state.needle.x, state.needle.y, 80, 40);
+   for (let i = 0; i < state.balls.length; i++) {
+      ctx.drawImage(sprites.ball, state.balls[i].x, state.balls[i].y, 30, 40);
+   }
+};
 
-	spawnInteral: function () {
-		setInterval( this.createBall, 3000)
-	},
+const update = function () {
+   if (state.needle.dx) {
+      state.needle.move();
+   }
+   for (let i = 0; i < state.balls.length; i++) {
+      if (state.balls[i].dy) {
+         state.balls[i].flight();
+      }
+   }
+};
 
-	update: function () {
-		if (this.state.needle.dx) {
-			this.state.needle.move();
-		}
-		for (let i = 0; i < this.state.balls.length; i++) {
-			if (this.state.balls[i].dy) {
-				this.state.balls[i].flight();
-			}
-		}
-	},
+let date = new Date();
 
-	run: function () {
-		this.update();
-		this.render();
-		window.requestAnimationFrame(function () {
-			game.run();
-		})
-	},
+const run = function () {
+   update();
+   if (new Date() - date > 500) {
+      createBall();
+      dellBall();
+      date = new Date();
+   }
+   render();
+   window.requestAnimationFrame(run);
 };
 
 window.addEventListener('load', function () {
-	game.start();
+   start();
 })
